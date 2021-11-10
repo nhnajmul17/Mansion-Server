db757b8a9bb45ddb36633b604a3817e37ede6dd4
nhnajmul17 <nhnajmul17@gmail.com>
2021-11-10 23:50:15 +0600

    apartments data

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zfz5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("mansion");
+        const reviewsCollection = database.collection("reviews");
+        const apartmentsCollection = database.collection("apartments");
+
+
+        app.get('/apartments', async (req, res) => {
+            const cursor = apartmentsCollection.find({});
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+    }
+    finally {
+        // await client.close()
+    }
+
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running MANSION SERVER')
+})
+
+app.listen(port, () => {
+    console.log('Running Server', port);
+})
